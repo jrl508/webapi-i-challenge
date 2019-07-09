9a9dd9070d7d51aa040e08a03d688f34e88629c3
jrl508 <jrl47@yahoo.com>
2019-07-09 01:58:50 -0400

    fully completed GET requests

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ server.get('/users', (req,res) => {
             res.status(200).json(users);
         })
         .catch(err =>{
-            res.status(500).json(err)
+            res.status(500).json({ error: "The users information could not be retrieved." })
         });
 });
 
@@ -29,7 +29,11 @@ server.get('/users/:id', (req,res) => {
     const { id } = req.params;
     db.findById(id)
         .then( user => {
-            res.status(200).json(user);
+            if(user){
+                res.status(200).json(user);
+            } else{
+                res.status(404).json({message: "The user with the specified ID does not exist."})
+            }
         })
         .catch(err =>{
             res.status(500).json(err)
